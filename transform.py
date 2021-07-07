c08e5893ce1f84e2245aa6aacae50995b8538c84
Joana <joanapiovaroli@gmail.com>
2021-07-07 00:07:22 -0300

    Add transform.py

---
+++ transform.py
@@ -0,0 +1,13 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+def list_to_row(data, column):
+    data[column] = data[column].str.split(',')
+    data = data.explode(column)
+    return data
+
+def group_by(data, column):
+    data.drop(['tconst'], axis=1, inplace=True)
+    data.drop_duplicates(inplace=True)
+    data = data.groupby(['startYear', 'genres']).agg({column: 'count'}).reset_index()
+    return data

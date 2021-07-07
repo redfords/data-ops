02c6be6a8612d61d5e64ea4fee5a1d60a1915e58
Joana <joanapiovaroli@gmail.com>
2021-07-07 01:22:27 -0300

    Replace file name with url

--- extract.py
+++ extract.py
@@ -1,12 +1,11 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None
 
-def extract_from_tsv(file_name, cols, dtypes):
-    file = file_name + ".tsv.gz"
-    df = pd.read_csv(file, compression='gzip', sep='\t', usecols=cols, dtype=dtypes)
+def extract_from_tsv(url, cols):
+    df = pd.read_csv(url, compression='gzip', sep='\t', usecols=cols)
     return df
 
-def create_df(file_name, cols):
+def create_df(url, cols):
     df = pd.DataFrame()
-    df = extract_from_tsv(file_name, cols)
+    df = extract_from_tsv(url, cols)
     return df

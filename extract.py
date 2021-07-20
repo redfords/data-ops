a0ced436ddcce5b4db1dbec0f4d86843da348975
Joana <joanapiovaroli@gmail.com>
2021-07-20 10:59:52 -0300

    Add file name

--- extract.py
+++ extract.py
@@ -5,7 +5,20 @@ def extract_from_tsv(url, cols):
     df = pd.read_csv(url, compression='gzip', sep='\t', usecols=cols)
     return df
 
-def create_df(url, cols):
+def create_df(file_name):
+    source_url = 'https://datasets.imdbws.com/'
+
+    files = {
+        'movie': ('title.basics', [0,1,5,7,8]),
+        'director': ('title.crew', [0,1]),
+        'writer': ('title.crew', [0,2]),
+        'ratings': ('title.ratings', [0,1,2]),
+        'name': ('name.basics', [0,1])
+    }
+
+    url = source_url + files[file_name][0]  + '.tsv.gz'
+    cols = files[file_name][1]
+
     df = pd.DataFrame()
     df = extract_from_tsv(url, cols)
     return df
